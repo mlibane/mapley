3c7db9f3c857cda92c7b02ccbf6a3f35c2f1c699
Libane <mlibane7@gmail.com>
2024-07-09 23:15:40 +0100

    Misc.

--- mapley/urls.py
+++ mapley/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('recipes.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('allauth.urls')),
 ]
